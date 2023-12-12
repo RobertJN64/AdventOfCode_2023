f215217ed4b1b5b65dec845fcd72eaff40635a03
RobertJN64 <robertjnies@gmail.com>
2023-12-12 00:59:44 -0500

    recursion

--- Day12/day12b.py
+++ Day12/day12b.py
@@ -1,68 +1,48 @@
 import util
 
-def is_valid(markers: str, numbers: list[int]):
-    numbers = [x for x in numbers] #copy
+def resolve_possibilties(item, numbers, number_index):
+    count = 0
 
-    c_count = 0
-    for m in markers:
-        if m == '#':
-            c_count += 1
-        if m == '.':
-            if c_count > 0:
-                if len(numbers) == 0 or numbers[0] != c_count:
-                    return False
-                numbers.pop(0)
+    item = item.strip('.')
+    if item[0] == '?' and len(item) > 1:
+        count += resolve_possibilties(item[1:], numbers, number_index)
 
-            c_count = 0
+    seq_len = numbers[number_index]
+    if len(item) < seq_len:
+        return count
+    if any([c == '.' for c in item[0:seq_len]]):
+        return count
+    if len(item) > seq_len and item[seq_len] == '#':
+        return count
 
-    if c_count > 0:
-        if len(numbers) == 0 or numbers[0] != c_count:
-            return False
-        numbers.pop(0)
-    if len(numbers) > 0:
-        return False
-    return True
-
-def quick_valid(markers, spring_count):
-    if markers.count('#') > spring_count:
-        return False
-    if markers.count("#") + markers.count("?") < spring_count:
-        return False
-    return True
+    new_item = item[seq_len + 1:]
+    if new_item == '' and number_index == len(numbers) - 1:
+        print(f"Found valid {item}")
+        return 1 + count
+    elif new_item == '' or number_index == len(numbers) - 1:
+        return count
+    else:
+        return count + resolve_possibilties(new_item, numbers, number_index + 1)
 
 
 def main():
-    with open("Day12/day12fast.txt") as f:
+    with open("Day12/day12.txt") as f:
         lines = [line.strip() for line in f.readlines()]
-    print(lines[0:10])
+    #print(lines[0:10])
 
     answer = 0
 
-    for i, line in enumerate(lines):
-        count = 0
+    for i, line in enumerate(lines[5:]):
+
         markers, numbers = line.split(' ')
         numbers = list(map(int, numbers.split(',')))
-        spring_count = sum(numbers)
-        #assert is_valid(markers, numbers), f"{markers=} {numbers=}"
 
-        queue: list[str] = [markers]
-        while queue:
-            item = queue.pop(0)
-            if '?' in item:
-                new_item = item.replace('?', '#', 1)
-                if quick_valid(new_item, spring_count):
-                    queue.append(new_item)
+        # markers: str = ((markers + '?') * 5)[:-1]
+        # numbers = list(map(int, numbers.split(','))) * 5
 
-                new_item = item.replace('?', '.', 1)
-                if quick_valid(new_item, spring_count):
-                    queue.append(new_item)
+        #spring_count = sum(numbers)
 
-            else:
-                if is_valid(item, numbers):
-                    #print(item, numbers)
-                    count += 1
-                # else:
-                #     print(item, numbers, 'x')
+        count = resolve_possibilties(markers, numbers, 0)
         answer += count
         print(i, count)
     print(answer)
