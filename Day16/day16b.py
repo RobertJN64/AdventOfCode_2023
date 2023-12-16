465539f886f0fe6a65c8acf437e3c583f7d0b49d
RobertJN64 <robertjnies@gmail.com>
2023-12-16 00:41:35 -0500

    part2 still somewhat slow

--- Day16/day16b.py
+++ Day16/day16b.py
@@ -12,7 +12,7 @@ mir_map = {
         {0: 2, 1: 3, 3: 1, 2: 0}
 }
 
-def add_laser(lasers, x, y, d, grid):
+def get_step(x, y, d):
     if d == 0:
         x += 1
     if d == 1:
@@ -21,9 +21,7 @@ def add_laser(lasers, x, y, d, grid):
         y += 1
     if d == 3:
         y -= 1
-    if util.out_of_bounds(grid, x, y):
-        return
-    lasers.append((x, y, d))
+    return x, y, d
 
 def score(grid, startx, starty, startd):
     energerized = []
@@ -38,34 +36,34 @@ def score(grid, startx, starty, startd):
 
     while lasers:
         # util.print_grid(energerized)
-
         x, y, d = lasers.pop(0)
-        if (x, y, d) in visited:
-            continue
-        visited.append((x, y, d))
-        energerized[y][x] = '#'
+        while not util.out_of_bounds(grid, x, y):
+            if (x, y, d) in visited:
+                break
+            visited.append((x, y, d))
+            energerized[y][x] = '#'
 
-        tile = grid[y][x]
+            tile = grid[y][x]
 
-        if tile == '.':
-            add_laser(lasers, x, y, d, grid)
-        elif tile in mir_map:
-            d = mir_map[grid[y][x]][d]
-            add_laser(lasers, x, y, d, grid)
-        elif tile == '|':
-            if d in [0, 1]:
-                add_laser(lasers, x, y, 2, grid)
-                add_laser(lasers, x, y, 3, grid)
+            if tile == '.':
+                x, y, d = get_step(x, y, d)
+            elif tile in mir_map:
+                d = mir_map[grid[y][x]][d]
+                x, y, d = get_step(x, y, d)
+            elif tile == '|':
+                if d in [0, 1]:
+                    x, y, d = get_step(x, y, 2)
+                    lasers.append(get_step(x, y, 3))
+                else:
+                    x, y, d = get_step(x, y, d)
+            elif tile == '-':
+                if d in [2, 3]:
+                    x, y, d = get_step(x, y, 0)
+                    lasers.append(get_step(x, y, 1))
+                else:
+                    x, y, d = get_step(x, y, d)
             else:
-                add_laser(lasers, x, y, d, grid)
-        elif tile == '-':
-            if d in [2, 3]:
-                add_laser(lasers, x, y, 0, grid)
-                add_laser(lasers, x, y, 1, grid)
-            else:
-                add_laser(lasers, x, y, d, grid)
-        else:
-            raise Exception("Invalid square")
+                raise Exception("Invalid square")
 
     answer = 0
     for row in energerized:
@@ -80,12 +78,12 @@ def main():
     print(grid[0:10])
 
     mscore = 0
-    for x in range(0, 10):
+    for x in range(0, len(grid[0])):
         print(x)
         mscore = max(mscore, score(grid, x, 0, 2))
         mscore = max(mscore, score(grid, x, len(grid)-1, 3))
 
-    for y in range(0, 10):
+    for y in range(0, len(grid)):
         print(y)
         mscore = max(mscore, score(grid, 0, y, 0))
         mscore = max(mscore, score(grid, len(grid[0])-1, y, 1))
