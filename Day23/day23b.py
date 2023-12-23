741e7e1f5e311bf1d1bd9affc0a1889715983c85
RobertJN64 <robertjnies@gmail.com>
2023-12-23 00:20:00 -0500

    a solved, b bruteforce is slow - either need pruning or solve with graph theory magic

--- Day23/day23b.py
+++ Day23/day23b.py
@@ -1,13 +1,40 @@
 import util
 
+
+
 def main():
     with open("Day23/day23.txt") as f:
-        lines = [line.strip() for line in f.readlines()]
-    print(lines[0:10])
+        grid = [list(line.strip()) for line in f.readlines()]
+
+    util.print_grid(grid)
+    queue = [(1, 0, {(1,0)})]
+
+    def add_pt(_x, _y, _hist: set):
+        if (_x, _y) in hist:
+            return
+        _hist = _hist.copy()
+        _hist.add((_x, _y))
+        queue.append((_x, _y, _hist))
+
+    best_len = 0
+    while queue:
+        if len(queue) % 1000 == 0:
+            print(len(queue))
+
+        x, y, hist = queue.pop(0)
+
+        if y == len(grid) - 1 and x == len(grid[0]) - 2:
+            print("Found new len: ", len(hist) - 1)
+            best_len = max(best_len, len(hist) - 1)
 
 
-    answer = 0
-    for line in lines:
-        answer += int(line)
+        for dx, dy in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+            if util.out_of_bounds(grid, x + dx, y + dy):
+                continue
 
-    print(answer)
+            if grid[y][x] == '#':
+                continue
+
+            add_pt(x+dx, y+dy, hist)
+
+    print("ANSWER", best_len)
