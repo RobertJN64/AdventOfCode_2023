7276a18f062d9b8ce5349300dd9dbab9b988ed91
RobertJN64 <robertjnies@gmail.com>
2023-12-24 14:16:22 -0500

    day18b solved using area given coord formula

--- Day18/day18b.py
+++ Day18/day18b.py
@@ -20,22 +20,27 @@ def main():
     trench_len = 0
 
     for direction, qty, color in cmds:
+        color = color.replace(')', '').replace('(#', '')
+        direction = color[-1:]
+        qty = int(color[:-1], 16)
+        print(qty, direction)
+
         qty = int(qty)
         trench_len += qty
-        if direction == 'R':
+        if direction in ['R', '0']:
             x += qty
 
-        elif direction == 'L':
+        elif direction in ['L', '2']:
             x -= qty
 
-        elif direction == 'D':
+        elif direction in ['D', '1']:
             y += qty
 
-        elif direction == 'U':
+        elif direction in ['U', '3']:
             y -= qty
 
         else:
-            raise Exception()
+            raise Exception(f"{direction=}")
 
         coords.append((x, y))
 
