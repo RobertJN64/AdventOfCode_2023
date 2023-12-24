e3dbac6abcc8545b3d312d9624a3b80ec018720b
RobertJN64 <robertjnies@gmail.com>
2023-12-24 14:01:19 -0500

    day24b not working

--- Day24/day24b.py
+++ Day24/day24b.py
@@ -24,15 +24,20 @@ def main():
 
     eqs = []
     timers = [5, 3, 4, 6, 1]
-    for index, stone in enumerate(stones[0:3]):
-        print(f"Adding stone {index}")
-        s_timer = symbols(f's{index}_timer')
-        #s_timer = timers[index]
-        t_eq_x = Eq((rock_x - stone.x), (s_timer * (stone.vx - rock_vx)))
-        t_eq_y = Eq((rock_y - stone.y), (s_timer * (stone.vy - rock_vy)))
-        t_eq_z = Eq((rock_z - stone.z), (s_timer * (stone.vz - rock_vz)))
-        eqs.extend([t_eq_x, t_eq_y, t_eq_z])
+    letters = 'abcdefg'
+    for index, stone in enumerate(stones):
+        # print(f"Adding stone {index}")
+        # s_timer = symbols(f's{letters[index]}_timer')
+        # #s_timer = timers[index]
+        # t_eq_x = Eq((rock_x - stone.x), (s_timer * (stone.vx - rock_vx)))
+        # t_eq_y = Eq((rock_y - stone.y), (s_timer * (stone.vy - rock_vy)))
+        # t_eq_z = Eq((rock_z - stone.z), (s_timer * (stone.vz - rock_vz)))
+        print(f"rockx - {stone.x} = s{index}timer * ({stone.vx} - rockvx)")
+        print(f"rocky - {stone.y} = s{index}timer * ({stone.vy} - rockvy)")
+        print(f"rockz - {stone.z} = s{index}timer * ({stone.vz} - rockvz)")
+        #eqs.extend([t_eq_x, t_eq_y, t_eq_z])
 
     for eq in eqs:
         print(eq)
-    print(nsolve(eqs, (rock_x, rock_y, rock_z, rock_vx, rock_vy, rock_vz), (0,0,0,0,0,0)))
+
+    #print(nsolve(eqs, (rock_x, rock_y, rock_z, rock_vx, rock_vy, rock_vz), (0,0,0,0,0,0)))
