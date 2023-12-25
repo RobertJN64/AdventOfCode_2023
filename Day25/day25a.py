3a38a7ba8452aded7968f379c98e5e28c3a21b9a
RobertJN64 <robertjnies@gmail.com>
2023-12-25 13:02:53 -0500

    wait - both sides of wire have to be in dif groups

--- Day25/day25a.py
+++ Day25/day25a.py
@@ -1,6 +1,8 @@
 import copy
 
+import line_profiler
 import util
+
 
 class Wire:
     def __init__(self, start, end):
@@ -30,28 +32,20 @@ def add_node(a: str, b: str, nodes: dict[str, Node]):
         nodes[b] = Node(b)
     nodes[b].add_connection(a)
 
+@line_profiler.profile
 def calc_group_size(nodes: dict[str, Node]):
-    found = []
-    for node in nodes:
-        for sublist in found:
-            if node in sublist:
-                break
+    start_node = list(nodes.keys())[0]
+    found = {start_node}
 
-        else:
-            t_f = [node]
-            queue = nodes[node].connections
-            while queue:
-                node = queue.pop(0)
-                if node not in t_f:
-                    t_f.append(node)
-                    queue.extend(nodes[node].connections)
-            found.append(t_f)
+    queue = [start_node]
+    while queue:
+        node = queue.pop(0)
+        for conn in nodes[node].connections:
+            if conn not in found:
+                found.add(conn)
+                queue.append(conn)
 
-    return [len(slist) for slist in found]
-
-def break_connection(wire: Wire, nodes: dict[str, Node]):
-    nodes[wire.a].connections.remove(wire.b)
-    nodes[wire.b].connections.remove(wire.a)
+    return len(found)
 
 def main():
     with open("Day25/day25.txt") as f:
@@ -59,7 +53,7 @@ def main():
     print(lines[0:10])
 
     wires = []
-    nodes = {}
+    nodes: dict[str, Node] = {}
     for line in lines:
         start, ends = line.split(': ')
         ends = ends.split()
@@ -68,23 +62,34 @@ def main():
 
             add_node(start, end, nodes)
 
+    #@util.debug_IO
+    def break_connection(wire: Wire):
+        nodes[wire.a].connections.remove(wire.b)
+        nodes[wire.b].connections.remove(wire.a)
+
+    #@util.debug_IO
+    def restore_connection(wire: Wire):
+        nodes[wire.a].connections.append(wire.b)
+        nodes[wire.b].connections.append(wire.a)
+
+    print(nodes)
+
     for aindex, wire_a in enumerate(wires):
-        print(aindex * 100 / len(wires))
+        break_connection(wire_a)
+
         for bindex, wire_b in enumerate(wires[aindex+1:]):
+            break_connection(wire_b)
+            print(aindex * 100 / len(wires), (aindex+bindex+1) * 100 / len(wires))
+
             for wire_c in wires[bindex+aindex+2:]:
-                t_nodes = copy.deepcopy(nodes)
-                break_connection(wire_a, t_nodes)
-                break_connection(wire_b, t_nodes)
-                break_connection(wire_c, t_nodes)
-                cgs = calc_group_size(t_nodes)
-                if len(cgs) == 2:
-                    print(cgs, wire_a, wire_b, wire_c)
-                    print(cgs[0] * cgs[1])
-                    raise Exception("ANSWER")
+                break_connection(wire_c)
+                size = calc_group_size(nodes)
+                restore_connection(wire_c)
 
-                #
-                # print(wire_a, wire_b, wire_c)
+                if size != len(nodes):
+                    print(wire_a, wire_b, wire_c)
+                    print(size * (len(nodes) - size))
+                    #raise Exception("ANSWER")
 
-    # print(wires)
-    # print(nodes)
-    # print(calc_group_size(nodes))
+            restore_connection(wire_b)
+        restore_connection(wire_a)
